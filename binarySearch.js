884ffb242e7f007554d921892e4e82873fa14d7f
KevinWang <kevinwang2020@sina.com>
2022-12-28 21:29:09 +0800

    Add binarySearch method

---
+++ binarySearch.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param {Array} arr
+ * @param {any} target
+ * @param {number} left
+ * @param {number} right
+ * @returns {number}
+ * O(Log2N)
+ */
+const search = (arr, target, left, right) => {
+    if (left > right) return -1;
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) return search(arr, target, mid + 1, right);
+    if (target < arr[mid]) return search(arr, target, left, mid - 1);
+};
+
+const bSearch = (arr, target) => {
+    return search(arr, target, 0, arr.length);
+};
