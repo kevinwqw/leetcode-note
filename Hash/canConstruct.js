f975965d96acdd36f7dc64c631d82afc3eb84187
KevinWang <kevinwang2020@sina.com>
2023-01-16 23:36:48 +0800

    Add canConstuct method

---
+++ Hash/canConstruct.js
@@ -0,0 +1,16 @@
+const canConstruct = (ransomNote, magazine) => {
+    const record = new Map();
+    for (const letter of magazine) {
+        record.set(letter, (record.get(letter) || 0) + 1);
+    }
+
+    for (const letter of ransomNote) {
+        if (!record.get(letter)) {
+            return false;
+        } else {
+            record.set(letter, record.get(letter) - 1);
+        }
+    }
+
+    return true;
+};
