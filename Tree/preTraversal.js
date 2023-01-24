06f63ddde2f74c7c94e21076c41d80d081672ba3
KevinWang <kevinwang2020@sina.com>
2023-01-24 15:25:40 +0800

    Add treeNode traversal method

---
+++ Tree/preTraversal.js
@@ -0,0 +1,17 @@
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = val || 0;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+/**
+ * Leetcode index: 144
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const preTraversal = (root) => {
+    t;
+};
