caf8d19032a7b215f0306341a7b5822ebbd4c364
KevinWang <wangqiwen15@crland.com.cn>
2024-02-05 14:47:20 +0800

    33. 搜索旋转排序数组

---
+++ favorite/33.搜索旋转排序数组.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let l = 0
+  let r = nums.length - 1
+  let mid
+
+  while (l <= r) {
+    mid = l + ((r - l) >> 1)
+
+    if (nums[mid] === target) return mid
+
+    if (nums[mid] >= nums[l]) {
+      // 左侧升序
+      if (target >= nums[l] && target < nums[mid]) {
+        r = mid - 1
+      } else {
+        l = mid + 1
+      }
+    } else {
+      // 右侧升序
+      if (target > nums[mid] && target <= nums[r]) {
+        l = mid + 1
+      } else {
+        r = mid - 1
+      }
+    }
+  }
+
+  return -1
+}
+// @lc code=end
